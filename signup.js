bbd31b089842f682fd9246aa2e3826e6a6a58cf4
murickananickal <98010645+murickananickal@users.noreply.github.com>
2022-02-02 20:42:24 +0530

    Email and password fields validation.
    Email and password field validations.

--- signup.js
+++ signup.js
@@ -1,5 +1,5 @@
-let form3Example3 = document.getElementById("form3Example3");
-let form3Example4 = document.getElementById("form3Example4");
+let form3Example3cg = document.getElementById("form3Example3cg");
+let form3Example4cg = document.getElementById("form3Example4cg");
 let error = document.getElementById("error")
 function validate(){
     // if(email.value=="" || pwd.value==""){
@@ -11,7 +11,7 @@ function validate(){
     //     //alert("Validation is proper")
     // }
     let regxp = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
-    if(regxp.test(form3Example3.value)){
+    if(regxp.test(form3Example3cg.value)){
         alert("Valid")
         //error.innerHTML = "Valid"
         //error.style.color="green"
@@ -24,18 +24,18 @@ function validate(){
         return false;
 
     }
-     if(form3Example3.value.trim()==""){
+     if(form3Example3cg.value.trim()==""){
          alert("Email cannot be empty")
          return false;
 
      }
-     else if(form3Example4.value.trim()==""){
+     else if(form3Example4cg.value.trim()==""){
          alert("Password cannot be empty")
          return false;
      }
-     else if(form3Example4.value.length<=5){
+     else if(form3Example4cg.value.length<=5){
          alert("Password is too short")
-         form3Example4.style.border="2px solid red"
+         form3Example4cg.style.border="2px solid red"
 
      }
      else{
